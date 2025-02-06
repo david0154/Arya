9fa95da3b0f7f88b8357f89a0c2ba78dbf3ed132
David <136182039+david0154@users.noreply.github.com>
2025-02-06 15:03:39 +0530

    Update install.sh

--- Installer/install.sh
+++ Installer/install.sh
@@ -5,13 +5,15 @@
 # Developer's Info
 DEVELOPER_EMAIL="davidk76011@gmail.com"
 DEVELOPER_ADDRESS="Kolkata, Salt Lake Sector 5, West Bengal, India 🇮🇳"
-LANGUAGES=("php" "python3" "nodejs" "nginx" "apache2" "openjdk-11-jdk" "golang" "gcc" "rustc")
+
+# Base languages to install
+LANGUAGES=("php" "python3" "nodejs" "nginx" "apache2" "openjdk-11-jdk" "golang" "gcc" "rustc" "composer" "npm")
 
 # Update package lists and upgrade the system
 echo "Updating system packages..."
 sudo apt update -y && sudo apt upgrade -y
 
-# Install required base languages and tools
+# Install required base languages and server tools
 echo "Installing required base languages and server tools..."
 for lang in "${LANGUAGES[@]}"; do
     echo "Installing $lang..."
@@ -34,7 +36,7 @@ sudo certbot --apache --non-interactive --agree-tos --email $DEVELOPER_EMAIL
 
 # Download Arya Language package (from GitHub or other repository)
 echo "Downloading Arya Language package..."
-git clone https://github.com/yourgithub/arya-language.git /usr/local/arya
+git clone https://github.com/david0154/Arya.git /usr/local/arya
 
 # Install Arya Language dependencies
 echo "Installing Arya dependencies..."
@@ -51,10 +53,45 @@ echo "Adding Arya to system PATH..."
 echo 'export PATH=$PATH:/usr/local/arya/bin' >> ~/.bashrc
 source ~/.bashrc
 
-# Verify installation
+# Verify Arya installation
 echo "Verifying Arya installation..."
 arya --version
 
+# Install Base Language Libraries for each language
+
+# PHP Libraries Installation
+echo "Installing all PHP Libraries..."
+cd /usr/local/arya/php
+composer global require laravel/installer symfony/console doctrine/orm phpunit/phpunit
+
+# Python Libraries Installation
+echo "Installing all Python Libraries..."
+cd /usr/local/arya/python
+pip install numpy pandas scikit-learn tensorflow keras flask django requests beautifulsoup4 matplotlib
+
+# Node.js Libraries Installation
+echo "Installing all Node.js Libraries..."
+cd /usr/local/arya/nodejs
+npm install express react lodash axios mongoose socket.io moment
+
+# Go Libraries Installation
+echo "Installing all Go Libraries..."
+cd /usr/local/arya/go
+go get github.com/gin-gonic/gin golang.org/x/tools github.com/sirupsen/logrus
+
+# Java Libraries Installation
+echo "Installing all Java Libraries..."
+cd /usr/local/arya/java
+mvn install org.springframework.boot:spring-boot-starter org.apache.commons:commons-lang3 org.hibernate:hibernate-core
+
+# Rust Libraries Installation
+echo "Installing all Rust Libraries..."
+cd /usr/local/arya/rust
+cargo install rocket actix-web serde serde_json
+
+echo "All base language libraries installed successfully!"
+
+# Installation completed
 echo "Installation completed successfully!"
 
 # Print developer info
